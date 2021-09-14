2293e05353051b8444be3986a075cada1cb28ae8
Dejan-Stojkoski <dejan_pp2005@live.com>
2021-09-14 13:45:32 +0200

    Class2 Homework Upload

---
+++ Class2/Class2/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Class2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Class2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("getAllUsers")]
+        public ActionResult<List<User>> GetUsers()
+        {
+            return StaticDB.Users;
+        }
+
+        [HttpGet("getUser/{userId}")]
+        public ActionResult<User> GetUserById(int userId)
+        {
+            var user = StaticDB.Users.FirstOrDefault(x => x.Id == userId);
+            if(user != null)
+            {
+                return user;
+            }
+
+            return NotFound($"User with ID:{userId} does not exist!");
+        }
+
+        [HttpGet("adultCheck/userId/{userId}")]
+        public ActionResult CheckIfUserIsAdult(int userId)
+        {
+            var user = StaticDB.Users.FirstOrDefault(x => x.Id == userId);
+            if (user != null)
+            {
+                var message = "";
+                message = user.Age > 17 ? $"The user with ID:{userId} is an adult!" : $"The user with ID:{userId} is not an adult!";
+
+                return Ok(message);
+            }
+
+            return NotFound($"User with ID:{userId} does not exist!");
+        }
+    }
+}
---
+++ Class2/Class2/StaticDB.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Class2.Models;
+
+namespace Class2
+{
+    public static class StaticDB
+    {
+        public static int UserId = 3;
+        public static List<User> Users = new List<User>()
+        {
+            new User
+            {
+                Id = 1,
+                FirstName = "Bill",
+                LastName = "Billsky",
+                Age = 17
+            },
+            new User
+            {
+                Id = 2,
+                FirstName = "Greg",
+                LastName = "Gregsky",
+                Age = 27
+            },
+            new User
+            {
+                Id = 3,
+                FirstName = "Jill",
+                LastName = "Jillsky",
+                Age = 32
+            }
+        };
+    }
+}
